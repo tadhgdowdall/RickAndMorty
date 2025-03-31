a94882f0eb2bf9162282e26a094c8c25169635bb
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2025-03-31 22:44:07 +0100

    added footer

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink, HeroSectionComponent, CardComponent],
+  imports: [CommonModule, RouterLink, HeroSectionComponent, CardComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
