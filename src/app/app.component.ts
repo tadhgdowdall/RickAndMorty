c3b9f9f91c12340d7309f31bd73a56f50d5884e2
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2025-03-31 20:03:54 +0100

    created hero section component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CardComponent],
+  imports: [RouterOutlet, NavbarComponent, CardComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
