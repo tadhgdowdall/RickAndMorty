6ba200945d00eccddbd7a270d4f66b268118a50b
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2025-05-07 11:16:26 +0100

    Created environment

--- src/app/services/favourites.service.ts
+++ src/app/services/favourites.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavouritesService {
 
-
-  private apiUrl = 'http://localhost:3000/api/favourites';
+  private apiUrl = `${environment.apiUrl}/favourites`;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://54.210.123.45:3000/api'
+  };
+  
